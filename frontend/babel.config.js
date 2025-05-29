17887b258310258876b4ac662645a5262cd22e51
savitender singh <paramsavi06@gmail.com>
2025-05-29 21:28:26 +0530

    Fix: Added custom babel plugin to handle PlatformConstants module

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -12,6 +12,31 @@ module.exports = function(api) {
           },
         },
       ],
+      // Add a custom plugin to provide PlatformConstants
+      function() {
+        return {
+          visitor: {
+            MemberExpression(path) {
+              if (
+                path.node.object.name === 'TurboModuleRegistry' &&
+                path.node.property.name === 'getEnforcing' &&
+                path.parent.arguments &&
+                path.parent.arguments[0] &&
+                path.parent.arguments[0].value === 'PlatformConstants'
+              ) {
+                const t = require('@babel/types');
+                path.parentPath.replaceWith(
+                  t.memberExpression(
+                    t.identifier('global'),
+                    t.identifier('PlatformConstants')
+                  )
+                );
+              }
+            },
+          },
+        };
+      },
+      'react-native-reanimated/plugin',
     ],
   };
 }; 
